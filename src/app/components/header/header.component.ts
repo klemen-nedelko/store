a9e1ae1f0154447852da3fb5f628bbc22ddf5bd7
Klemen <klemen.nedelko2@gmail.com>
2023-04-01 12:25:00 +0200

    added cart and cart page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  private _cart: Cart = { items: []};
+  itemsQuantity = 0;
+  @Input()
+  get cart(): Cart{ 
+    return this._cart;
+  }
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items.map((item) => item.quantity).reduce((prev, curr) => prev + curr, 0);
+  }
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+  onClearCart():void{
+    this.cartService.clearCart();
+  }
+
 
 }
